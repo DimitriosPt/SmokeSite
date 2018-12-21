56e8f5811d2699a1fbe6c66a5d98759b5f7b8100
Dimitrios <ptdimitrios@gmail.com>
2018-12-21 11:28:01 -0800

    separates map initialization into another file to clear up JS

--- js/index.js
+++ js/index.js
@@ -1,18 +1,55 @@
 let aqiDiv = document.getElementById("AQI");
-let latitude = 38.23;
-let longitude = -122.63;
 
 // api.airvisual.com/v2/nearest_city?lat={{LATITUDE}}&lon={{LONGITUDE}}&key={{YOUR_API_KEY}}
-let apiLatLngCall = "/api/visualair/38.23/-122.2"
+//let apiLatLngCall = "/api/visualair/38.23/-122.2";
+
+let button = document.createElement("button");
+
+let t = document.createTextNode("run");
+
+button.appendChild(t);
+
+document.body.appendChild(button);
+
+function setAirVisual_InfoWindow(coordinates)
+{
+    let nearestSensorWindow = new google.maps.InfoWindow({
+        content: "Nearest AirVisual Sensor",
+        position: coordinates
+    });
+
+    nearestSensorWindow.open(map);
+
+}
 
 const userAction = async () => {
-    const response = await fetch(apiLatLngCall);
-    const myJson = await response.json(); //extract JSON from the http response
+    let apiLatLngCall = '/api/visualair/${latitude}/${longitude}';
 
-    aqiDiv = myJson;
-    console.log(myJson);
-    $("#state").text(myJson.data.state);
-}
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+        });
+        const response = await fetch(apiLatLngCall);
+        const myJson = await response.json(); //extract JSON from the http response
+
+        console.log(myJson);
+        $("#state").text(myJson.data.city);
+        $("#AQI").text(myJson.data.current.pollution.aqius);
+
+        let airVisualSensorCoords = {
+            lat: myJson.data.location.coordinates[1],
+            lng: myJson.data.location.coordinates[0]
+        };
+        setAirVisual_InfoWindow(airVisualSensorCoords)
+    }
+};
+
+
+button.addEventListener("click", userAction);
+
 
 // activate with env\scripts\activate
 // launch with .\server.py
